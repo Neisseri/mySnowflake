c1d5b0b2a65fd6b3832f89ddcb7a1aff0be89f28
Neisseri <neissertong@gmail.com>
2023-08-24 17:06:32 +0800

    improve: kernel

--- function.h
+++ function.h
@@ -3,23 +3,26 @@
     #include"./paras.h"
 
     __global__ void kernel1_pure(highprecision *phi,highprecision* phi_lap,highprecision* tempr,highprecision* tempr_lap,highprecision* phidx,highprecision* phidy,highprecision* epsilon,highprecision* epsilon_deri){
-        int unitindex_x=blockIdx.z%unitdimX;
-        int unitindex_y=blockIdx.z/unitdimX;
+        int unitindex_x = blockIdx.z % unitdimX;
+        int unitindex_y = blockIdx.z / unitdimX;
         // 计算线程块在二维线程网络中的索引
-        int x_offset=threadIdx.x ;
-        int x_start=unitindex_x*unitx;
-        int x=x_start+x_offset;
+        int x_offset = threadIdx.x;
+        int x_start = unitindex_x * unitx;
+        int x = x_start + x_offset;
         // 计算线程在当前线程块内的 x 轴方向的索引
         // 和在整个线程网格中的 x 轴方向的绝对索引
-        int y_offset=threadIdx.y;
-        int y=unitindex_y*unity+y_offset;
+        int y_offset = threadIdx.y;
+        int y = unitindex_y * unity + y_offset;
 
-        int xs1=x>0?x-1:dimX-1;int ys1=y>0?y-1:dimY-1; // s1 : (x - 1, y - 1)
-        int xa1=x<dimX-1?x+1:0;int ya1=y<dimY-1?y+1:0; // a1: (x + 1, y + 1)
-        int xs2=x-2<0?x-2+dimX:x-2;
-        int xa2=x+2>dimX-1?x+2-dimX:x+2;
-        int ys2=y-2<0?y-2+dimY:y-2; // s2: (x - 2, y - 2)
-        int ya2=y+2>dimY-1?y+2-dimY:y+2; // a2: (x + 2, y + 2)
+        int xs1 = x > 0 ? (x - 1) : (dimX - 1);
+        int ys1 = y > 0 ? (y - 1) : (dimY - 1); // s1: (x - 1, y - 1)
+        int xa1 = x < (dimX - 1) ? (x + 1) : 0;
+        int ya1 = y < (dimY - 1) ? (y + 1) : 0; // a1: (x + 1, y + 1)
+        int xs2 = (x -2) < 0 ? (x - 2) + dimX : (x - 2);
+        int xa2 = (x + 2) > (dimX - 1) ? (x + 2 - dimX) : (x + 2);
+        int ys2 = (y - 2) < 0 ? (y - 2 + dimY) : (y - 2); // s2: (x - 2, y - 2)
+        int ya2 = (y + 2) > (dimY - 1) ? (y + 2 - dimY) :(y + 2); // a2: (x + 2, y + 2)
+        
         highprecision(*phid)[dimX]=(highprecision(*)[dimX])phi;
         highprecision(*phi_lapd)[dimX]=(highprecision(*)[dimX])phi_lap;
         highprecision(*phidxd)[dimX]=(highprecision(*)[dimX])phidx;
@@ -29,15 +32,37 @@
         highprecision(*epsilond)[dimX]=(highprecision(*)[dimX])epsilon;
         highprecision(*epsilon_derid)[dimX]=(highprecision(*)[dimX])epsilon_deri;
         // 将一维指针转换为二维数组
-        phi_lapd[y][x]=phid[y][xs1]/dxdy+phid[y][xs2]/dxdy+phid[y][xa1]/dxdy+phid[y][xa2]/dxdy+phid[ys1][x]/dxdy+phid[ys2][x]/dxdy+phid[ya1][x]/dxdy+phid[ya2][x]/dxdy-8.0*phid[y][x]/dxdy;
-        tempr_lapd[y][x]=temprd[y][xs1]/dxdy+temprd[y][xs2]/dxdy+temprd[y][xa1]/dxdy+temprd[y][xa2]/dxdy+temprd[ys1][x]/dxdy+temprd[ys2][x]/dxdy+temprd[ya1][x]/dxdy+temprd[ya2][x]/dxdy-8.0*temprd[y][x]/dxdy;
+        
+        phi_lapd[y][x] = 
+            phid[y][xs1] / dxdy
+            + phid[y][xs2] / dxdy
+            + phid[y][xa1] / dxdy
+            + phid[y][xa2] / dxdy
+            + phid[ys1][x] / dxdy
+            + phid[ys2][x] / dxdy
+            + phid[ya1][x] / dxdy
+            + phid[ya2][x] / dxdy
+            - 8.0 * phid[y][x] / dxdy;
 
-        phidxd[y][x]=(phid[y][xa1]-phid[y][xs1])/(2.0*dx);
-        phidyd[y][x]=(phid[ya1][x]-phid[ys1][x])/(2.0*dy);
-        highprecision theta=atan2(phidyd[y][x],phidxd[y][x]);
-        epsilond[y][x]=epsilonb*(1.0+delta*cos(aniso*(theta-theta0)));
-        epsilon_derid[y][x]=-epsilonb*aniso*delta*sin(aniso*(theta-theta0)); // UNKNOWN
+        tempr_lapd[y][x] = 
+            temprd[y][xs1] / dxdy
+            + temprd[y][xs2] / dxdy
+            + temprd[y][xa1] / dxdy
+            + temprd[y][xa2] / dxdy
+            + temprd[ys1][x] / dxdy
+            + temprd[ys2][x] / dxdy
+            + temprd[ya1][x] / dxdy
+            + temprd[ya2][x] / dxdy
+            - 8.0 * temprd[y][x] / dxdy;
+
+        phidxd[y][x] = (phid[y][xa1] - phid[y][xs1]) / (2.0 * dx);
+        phidyd[y][x] = (phid[ya1][x] - phid[ys1][x]) / (2.0 * dy);
+
+        highprecision theta = atan2(phidyd[y][x], phidxd[y][x]);
+        epsilond[y][x] = epsilonb * (1.0 + delta * cos(aniso * (theta - theta0)));
+        epsilon_derid[y][x] = -epsilonb * aniso * delta * sin(aniso * (theta - theta0));
     }
+
     __global__ void kernel2_pure(highprecision* phi,highprecision* phi_lap,highprecision* epsilon,highprecision *epsilon_deri,highprecision* phidx,highprecision* phidy,highprecision* tempr,highprecision* tempr_lap){
         int unitindex_x=blockIdx.z%unitdimX;
         int unitindex_y=blockIdx.z/unitdimX;
